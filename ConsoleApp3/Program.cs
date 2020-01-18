d3790eca06c543ecc9ebd03dca386b98ae98e1c4
Fifanncher <fifanncher@gmail.com>
2020-01-18 19:00:01 +0500

    Add proxy support

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,17 +9,22 @@ using Microsoft.SmallBasic.Library;
 using Telegram.Bot;
 using Telegram.Bot.Types.ReplyMarkups;
 using System.Web.Helpers;
+using MihaZupan;
 
-namespace TelegaCinemaBot
+namespace TelegaCinemaRouletteBot
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var proxy = new HttpToSocks5Proxy("217.61.130.19", 29202);
+            // Some proxies limit target connections to a single IP address
+            // If that is the case you have to resolve hostnames locally
+           // proxy.ResolveHostnamesLocally = true;
+            TelegramBotClient bot = new TelegramBotClient("974790258:AAFZtdELBh25LpaAjguUNtrl7B5OPX5g6Ek", proxy);
 
-            string token = ""; // Телеграм токен бота
-
-            var bot = new TelegramBotClient(token);
+            //string token = "974790258:AAFZtdELBh25LpaAjguUNtrl7B5OPX5g6Ek"; // Телеграм токен бота
+            //var bot = new TelegramBotClient(token);
 
             var markup = new ReplyKeyboardMarkup(new[]
              {
